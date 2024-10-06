2b31b98bc05283c0693b983271cd37dd1d3be7d0
Sai Kaushik Sadu <91894475+SaiKaushikSadu@users.noreply.github.com>
2024-10-06 14:10:34 +0530

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Product from "./pages/Product/Product";
-import Home from "./pages/Home/Home";
-import IndividualProduct from "./pages/IndividualProduct/IndividualProduct";
-import Checkout from "./pages/Checkout/Checkout";
-import Payment from "./pages/Paypal/Payment";
+// import Navbar from "./components/Navbar/Navbar";
+// import Product from "./pages/Product/Product";
+// import Home from "./pages/Home/Home";
+// import IndividualProduct from "./pages/IndividualProduct/IndividualProduct";
+// import Checkout from "./pages/Checkout/Checkout";
+// import Payment from "./pages/Paypal/Payment";
+
+const Navbar = lazy(() => import("./components/Navbar/Navbar"));
+const Product = lazy(() => import("./pages/Product/Product"));
+const Home = lazy(() => import("./pages/Home/Home"));
+const IndividualProduct = lazy(() => import("./pages/IndividualProduct/IndividualProduct"));
+const Checkout = lazy(() => import("./pages/Checkout/Checkout"));
+const Payment = lazy(() => import("./pages/Paypal/Payment"));
+const NotFound = lazy(() => import("./pages/NotFound/NotFound")); // Optional
+
+
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/products" element={<Product></Product>}></Route>
-          <Route path="/product/:id" element={<IndividualProduct></IndividualProduct>}></Route>
-          <Route path="/checkout" element={<Checkout></Checkout>}></Route>
-          <Route path="/payment" element={<Payment></Payment>}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/product/:id" element={<IndividualProduct />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <>
+        <div className="notfound"
+        style={{display:'flex', justifyContent:'center', alignItems:'center'}}
+        >
+            <img
+                src='https://www.digitalmesh.com/blog/wp-content/uploads/2020/05/404-error.jpg'
+                style={{width:'100vw', height:'90vh'}}
+            >
+            </img>
+        </div>
+        </>
+    )
+}
+
+export default NotFound
